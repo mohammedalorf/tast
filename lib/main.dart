a2ac675b43fe4b34a05140bdfd4bb21a6ec27e5e
Mohammed Alorf <mohammed1alorf@gmail.com>
2023-01-09 03:56:15 +0300

    hola

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'hi',
+      title: 'hola',
       theme: ThemeData(
         // This is the theme of your application.
         //
