d7bb53e4d45391a541852146cc43396fcb9d9dda
Mohammed Alorf <mohammed1alorf@gmail.com>
2023-01-17 01:43:23 +0300

    stateful button app

--- lib/main.dart
+++ lib/main.dart
@@ -4,32 +4,46 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int questaionIndex = 0;
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    var Questaions = [
+      'Are you a pota?',
+      'Are you gonzalis?',
+    ];
+    void ansQ() {
+      setState(() {
+        questaionIndex = questaionIndex + 1;
+      });
+
+      print(questaionIndex);
+    }
+
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text('hola'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('hola'),
+        ),
+        body: Column(
+          children: [
+            Text(Questaions[questaionIndex]),
+            ElevatedButton(
+              child: Text('first choese'),
+              onPressed: ansQ,
+            ),
+            ElevatedButton(child: Text('seconed choese'), onPressed: ansQ),
+            ElevatedButton(child: Text('third choese'), onPressed: ansQ),
+            ElevatedButton(child: Text('four choese'), onPressed: ansQ),
+          ],
+        ),
       ),
-      body: Column(children: [
-        Text('questaion ?'),
-        ElevatedButton(
-            child: Text('first choese'),
-            onPressed: () => print("button 1 has pressed")),
-        ElevatedButton(
-            child: Text('seconed choese'),
-            onPressed: () {
-              print('button 2 has pressed');
-            }),
-        ElevatedButton(
-            child: Text('third choese'),
-            onPressed: () => print("button 3 has been pressed")),
-        ElevatedButton(
-            child: Text('four choese'),
-            onPressed: () => print('button 4 has been pressed')),
-      ]),
-    ));
+    );
   }
 }
